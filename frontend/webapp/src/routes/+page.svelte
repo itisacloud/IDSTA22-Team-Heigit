f42d7185c490e9823ea3b9c92d0fa4c378f0853d
Gigaszi <74006329+Gigaszi@users.noreply.github.com>
2023-03-02 20:23:33 +0100

    redesign web page

--- frontend/webapp/src/routes/+page.svelte
+++ frontend/webapp/src/routes/+page.svelte
@@ -3,13 +3,11 @@
   import SearchBar from "/src/components/SearchBar.svelte";
   import SearchResults from "/src/components/SearchResults.svelte";
   import Map from "/src/components/Map.svelte";
-  import SelectLevel from "/src/components/SelectLevel.svelte";
+  import ResultSection from "/src/components/ResultSection.svelte";
 </script>
 
 <div class="flex flex-col justify-center items-center h-screen w-screen gap-y-4">
-  <Logo text="DemoSearch"/>
-  <SearchBar />
-  <SearchResults />
+  <Logo text="Sentiment Analysis for Tweets on the War in Ukraine"/>
   <Map />
 
 </div>
