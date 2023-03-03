68d44e84607f29cddcc352341b99328cf831fe3f
Gigaszi <74006329+Gigaszi@users.noreply.github.com>
2023-03-03 14:54:21 +0100

    add: new plor creation

--- code/Python/api.py
+++ code/Python/api.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import Optional
 from connections import elasticSearchConnection as es
 import utils_api
+import json
 from fastapi.responses import JSONResponse
 from main import readConfig
 
@@ -51,48 +52,64 @@ async def getPlots(item: Item):
         "entities": entities.tolist()
     }
 
-    """traces = []
+    def sort_entities(row):
+        for outer_key in row:
+            for inner_key in row[1]:
+                print(inner_key)
+                if inner_key == "n":
+                    return row[1][inner_key]
+
+    def square(row):
+
+        sorted_dict = sorted(row.items(), key=sort_entities, reverse=True)
+        cnt = 0
+        top_entities = []
+        for tup in sorted_dict:
+            if cnt < 5:
+                top_entities.append(tup[0])
+                cnt += 1
+        return top_entities
+
+    traces = []
     df = pd.DataFrame.from_dict(response)
     # Create figure with secondary y-axis
     fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-    # Add traces
-
     fig.add_trace(
-        go.Bar(y=df["counts"], name="yaxis2 data", hoverinfo='skip',
+        go.Bar(x=df["timestamps"], y=df["counts"], name="yaxis2 data", hoverinfo='skip',
                marker={'color': '#FFD700'}),
-        secondary_y=True,
+        secondary_y=False,
     )
 
     fig.add_trace(
         go.Scatter(
-            # x=df["timestamps"],
+            x=df["timestamps"],
             y=df["sentiment"],
-            customdata=np.stack((df['entities'], df['counts']), axis=-1),
+            customdata=np.stack((df['top_entities'], df['counts']), axis=-1),
             hovertemplate='<br>'.join([
-                'Sentiment: $%{y}',
-                'Tweet Count: %{customdata[1]}'
+                'Sentiment: %{y}',
+                'Tweet Count: %{customdata[1]}',
                 'Entitites : %{customdata[0]}',
             ]),
             line={'color': '#0057B8', "width": 5}),
-        secondary_y=False,
+        secondary_y=True,
     )
-    fig['layout']['yaxis']['autorange'] = "reversed"
+
+    # fig['layout']['yaxis']['autorange'] = "reversed"
+
     # Add figure title
     fig.update_layout(
         title_text="Twitter Sentiment Analysis",
         bargap=0.0,
         plot_bgcolor='rgb(207, 226, 243)'
     )
+
     # Set x-axis title
     fig.update_xaxes(title_text="Date")
+
     # Set y-axes titles
     fig.update_yaxes(title_text="Sentiment", secondary_y=False)
     fig.update_yaxes(title_text="Tweet Count", secondary_y=True)
-    fig.show()
 
-    fig.to_json()
     plot_json = pio.to_json(fig)
-
-    response["plot"] = json.dumps(json.loads(plot_json))"""
+    parsed = json.loads(plot_json)
     return JSONResponse(response)
