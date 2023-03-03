61560776a4de026aa93e37f44fd470e6a43c728e
Gigaszi <74006329+Gigaszi@users.noreply.github.com>
2023-03-03 17:51:28 +0100

    add: request API for plot creation

--- frontend/webapp/src/components/PlotlyGraph.svelte
+++ frontend/webapp/src/components/PlotlyGraph.svelte
@@ -1,26 +1,41 @@
 <script>
-  import Plotly from 'plotly.js-dist';
-  import { onMount, onDestroy } from 'svelte';
+    import Plotly from 'plotly.js-dist';
+    import {onMount, onDestroy} from 'svelte';
+    import {timeInterval, selectedRegions, selectedLayers} from "/src/components/Map.svelte";
+    import axios from "axios";
+    import {timeIntervall} from "./Map.svelte";
 
-  let graphData = null;
-  let graphDiv;
-  let graph = null;
+    let graphData = null;
+    let graphDiv;
+    let graph = null;
 
-  function loadGraphData() {
-    fetch('/src/components/test2.json')
-      .then(response => response.json())
-      .then(data => {
-        graphData = data;
-        graph = 1;
-        if (graph !== null) {
-          Plotly.newPlot(graphDiv, graphData.data, graphData.layout);
+    async function loadGraphData() {
+        let request_data = {"interval": timeIntervall, "layer": selectedLayers, "name": selectedRegions}
+        try {
+            let {data, status} = await axios.post('http://localhost:2999/plot', request_data);
+            if (status != 200) {
+                alert('Error fetching search results!');
+                return;
+            }
+            graphData = data;
+            graph = 1;
+            if (graph !== null) {
+                Plotly.newPlot(graphDiv, graphData.data, graphData.layout);
+            }
         }
-      });
-  }
+        catch
+            (error)
+            {
+                alert('Error fetching search results! Probably the API is not ready yet.');
+                return;
+            }
 
-  onMount(() => {
-    loadGraphData();
-  });
+        }
+
+
+    onMount(() => {
+        loadGraphData();
+    });
 
 
 </script>
@@ -28,4 +43,4 @@
 <button on:click={loadGraphData}>Reload Graph</button>
 
 
-  <div bind:this={graphDiv}></div>
+<div bind:this={graphDiv}></div>
