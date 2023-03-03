38fc53f003fcec57986793bd4e755b81a0e43bd9
Gigaszi <74006329+Gigaszi@users.noreply.github.com>
2023-03-03 17:35:54 +0100

    add: mocked webpage

---
+++ frontend/webapp/src/components/PlotlyGraph.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Plotly from 'plotly.js-dist';
+  import { onMount, onDestroy } from 'svelte';
+
+  let graphData = null;
+  let graphDiv;
+  let graph = null;
+
+  function loadGraphData() {
+    fetch('/src/components/test2.json')
+      .then(response => response.json())
+      .then(data => {
+        graphData = data;
+        graph = 1;
+        if (graph !== null) {
+          Plotly.newPlot(graphDiv, graphData.data, graphData.layout);
+        }
+      });
+  }
+
+  onMount(() => {
+    loadGraphData();
+  });
+
+
+</script>
+
+<button on:click={loadGraphData}>Reload Graph</button>
+
+
+  <div bind:this={graphDiv}></div>
