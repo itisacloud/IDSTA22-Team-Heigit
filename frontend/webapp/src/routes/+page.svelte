2b803cb8a38b92cbccd7490603adbb66c1ae9bf4
Gigaszi <74006329+Gigaszi@users.noreply.github.com>
2023-03-03 17:34:42 +0100

    add: styling webpage layout

--- frontend/webapp/src/routes/+page.svelte
+++ frontend/webapp/src/routes/+page.svelte
@@ -4,10 +4,14 @@
   import SearchResults from "/src/components/SearchResults.svelte";
   import Map from "/src/components/Map.svelte";
   import ResultSection from "/src/components/ResultSection.svelte";
+  import PlotlyGraph from '/src/components/PlotlyGraph.svelte';
+
 </script>
 
-<div class="flex flex-col justify-center items-center h-screen w-screen gap-y-4">
+<div class="flex flex-col items-center justify-center h-screen w-screen gap-y-4">
   <Logo text="Sentiment Analysis for Tweets on the War in Ukraine"/>
   <Map />
-
 </div>
+<div class="flex flex-col items-center w-screen ">
+  <PlotlyGraph />
+  </div>
